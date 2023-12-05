9cef98a3a9089c1e97638b146b957cdc20e1392a
William Horvat <wahorvat@gmail.com>
2023-12-05 13:35:45 -0500

    puzzle 2

---
+++ python/pz_02.py
@@ -0,0 +1,40 @@
+import string
+import parse
+import math
+from collections.abc import Iterator
+
+input_file = open("./input/input_02.txt") 
+data = input_file.read()
+lines = data.splitlines()
+
+type Line_Maxes = dict[str, int]
+
+def find_color(line: str, color: str) -> Iterator[int]:
+    return (match["num"] for match in parse.findall(f"{{num:d}} {color}", line))
+
+def line_maxes(line: str) -> Line_Maxes:
+   return {color: max(find_color(line, color)) for color in ("red", "green", "blue")}
+
+def game_num(line: str) -> int:
+    return parse.search("Game {game_num:d}", line)["game_num"]
+
+result1 = 0
+for line in lines:
+    maxes = line_maxes(line)
+    game_val = game_num(line)
+    if (maxes['red'] > 12 or maxes['green'] > 13 or maxes['blue'] > 14):
+        pass
+    else:
+        result1 += game_val
+
+
+def set_power(maxes: dict) -> int:
+    return math.prod(maxes.values())
+
+result2 = 0
+for line in lines:
+    maxes = line_maxes(line)
+    prod = set_power(maxes)
+    result2 += prod
+
+print(result2)
