9ef4d547a4504a6c84e7ee70efa1ad0bfd761833
wahorvat <wahorvat@gmail.com>
2024-03-03 22:38:58 -0500

    day 12

--- python/pz_12.py
+++ python/pz_12.py
@@ -12,9 +12,27 @@ test_string = """???.### 1,1,3
 ?###???????? 3,2,1"""
 
 lines = test_string.splitlines()
-print(int(lines[0].split()[1][0]))
+print(lines[0])
 
-"""permutations of spaced chuncks that fit the number considering existing characters"""
+def count_arrangements(input_string):
+    parts = input_string.split()
+    sequence = list(parts[0])
+    lengths = list(map(int, parts[1].split(',')))
+    print(f'\n lengths: {lengths}', f'sequence: {sequence} \n')
 
+    total_arrangements = 1  
+    
+    for length in lengths:
+        count = sequence.count('?') + sequence.count('#')
+        if count < length:
+            return 0
+        arrangements = 1
+        for i in range(length):
+            arrangements *= count - i
+        total_arrangements *= arrangements
+        sequence = sequence[length+1:]  # remove the used characters and a period
 
+    return total_arrangements
 
+# Test the function
+print(count_arrangements('???.### 1,1,3'))  # Output: 0
